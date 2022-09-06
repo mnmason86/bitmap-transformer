a018787e24be0ad9673b0ad40a6aa3fc7cf229f5
Mandy Mason <102629678+mnmason86@users.noreply.github.com>
2022-09-05 20:49:31 -0400

    Update App.java

--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -4,13 +4,30 @@
 package bitmap.transformer;
 
 public class App {
-
-    public static void main(String[] args) {
-        BitMap x = new BitMap("C:\\msys64\\home\\mnmas\\bitmap-transformer\\app\\src\\main\\resources\\baldy-8bit.bmp", "C:\\msys64\\home\\mnmas\\bitmap-transformer\\app\\src\\main\\resources\\altered-image.bmp", "reverseColor");
-        x.readFile();
-        x.greyScale();
-        x.writeFile();
-        x.verticalReverse();
-        x.greenScale();
+    String rootPath = System.getProperty("user.dir");
+        String resourcePath = "";
+        if (rootPath.endsWith("bitmap-transformer")){
+            resourcePath = "app\\src\\main\\resources\\";
+        } else {
+            resourcePath = "src\\main\\resources\\";
+        }
+        String inputFilePath = resourcePath + args[0],
+                outputFilePath = resourcePath + args[1],
+                transformName = args[2];
+        BitMap alteredImage = new BitMap(inputFilePath, outputFilePath,
+                transformName);
+        if (transformName.equals("grey-scale")){
+            alteredImage.readFile();
+            alteredImage.greyScale();
+        }
+        if (transformName.equals("vertical-reverse")){
+            alteredImage.readFile();
+            alteredImage.verticalReverse();
+        }
+        if (transformName.equals("green-scale")){
+            alteredImage.readFile();
+            alteredImage.greenScale();
+        }
+        alteredImage.writeFile();
     }
 }
